9310745618acd914a875f0150f9dd26acd7b8a66
gonza.fl <gonzaloflamenci4@gmail.com>
2022-10-25 01:30:07 -0300

    fix navbar postion

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -3,7 +3,7 @@ const Nav = ({menuVisible}) => {
     <nav
       className={`fixed bg-white w-[80%] md:w-[40%] xl:w-full h-full ${
         !menuVisible ? '-left-full' : 'left-0'
-      } top-0 xl:static flex-1 flex justify-center xl:justify-start ml-[23%] items-center transition-all duration-1000`}
+      } top-0 xl:static flex-1 flex justify-center xl:justify-start xl:ml-[23%] items-center transition-all duration-1000`}
     >
       <ul className='flex flex-col xl:flex-row gap-10 font-medium text-center w-[30%] justify-around'>
         <li>
