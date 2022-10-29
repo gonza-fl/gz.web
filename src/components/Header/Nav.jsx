0c48303d43bab31abc2282b7c0100a2a5e1f878e
gonza.fl <gonzaloflamenci4@gmail.com>
2022-10-29 04:24:04 -0300

    fix navbar to desktop screen

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -3,9 +3,9 @@ const Nav = ({ menuVisible }) => {
     <nav
       className={`fixed bg-white w-[80%] md:w-[40%] xl:w-full h-full ${
         !menuVisible ? '-left-full' : 'left-0'
-      } top-0 xl:static flex-1 flex flex-col justify-center xl:justify-start xl:ml-[23%] items-center transition-all duration-1000`}
+      } top-0 xl:static flex-1 flex flex-col xl:flex-row justify-center xl:justify-around xl:ml-[23%] items-center transition-all duration-1000`}
     >
-      <ul className='mb-20 lg:mb-0 flex justify-center w-[80%]'>
+      <ul className='mb-20 lg:mb-0 flex justify-center -xl:w-[80%]'>
         <li className='mr-5'>
           <a href='https://github.com/gonza-fl' target='_blank'>
             <i class='nes-icon github is-medium'></i>
