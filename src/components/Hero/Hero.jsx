adf4fa8d3880f77254d48f9f3c8032b2cc0ab68c
gonza.fl <gonzaloflamenci4@gmail.com>
2023-03-11 19:04:27 -0300

    deploy images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
-import Bg from '../../assets/bg3.jpg';
 import ProfileCard from '../ProfileCard/ProfileCard';
 
 const Hero = () => {
   return (
-    <section className='h-[90vh] bg-cover bg-center' style={{ backgroundImage: `url(${Bg})` }}>
+    <section
+      className='h-[90vh] bg-cover bg-center'
+      style={{ backgroundImage: 'url(https://i.postimg.cc/SK0CQ8xG/bg3.jpg)' }}
+    >
       <header className='text-white  flex justify-center pt-10'>
-        <ProfileCard/>
+        <ProfileCard />
       </header>
     </section>
   );
--- src/components/ProfileCard/PhotoProfile.jsx
+++ src/components/ProfileCard/PhotoProfile.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ProfilePhoto from '../../assets/gdt.png';
 
 const PhotoProfile = () => {
   return (
     <div
-      style={{ backgroundImage: `url(${ProfilePhoto})` }}
+      style={{ backgroundImage: 'url(https://i.postimg.cc/NGb685dg/gdt.png)' }}
       className='w-[80%] h-[50%] md:w-96 bg-cover bg-center'
     ></div>
   );
