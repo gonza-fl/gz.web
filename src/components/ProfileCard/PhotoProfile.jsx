53ce8195ffb7421f9c016eff3326ac72752373b5
gonza.fl <gonzaloflamenci4@gmail.com>
2023-03-12 03:22:35 -0300

    repair image-profile

--- src/components/ProfileCard/PhotoProfile.jsx
+++ src/components/ProfileCard/PhotoProfile.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 const PhotoProfile = () => {
   return (
-    <div
-      style={{ backgroundImage: 'url(https://i.postimg.cc/NGb685dg/gdt.png)' }}
-      className='w-[80%] h-[50%] md:w-96 bg-cover bg-center'
-    ></div>
+    <img src='https://i.postimg.cc/NGb685dg/gdt.png' className='md:w-96'></img>
   );
 };
 
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -3,7 +3,7 @@ import PhotoProfile from './PhotoProfile';
 
 const ProfileCard = () => {
   return (
-    <div className='backdrop-blur-sm h-[70vh] md:h-[78vh] w-[80%] flex flex-col lg:flex-row-reverse px-2 justify-around items-center bg-gray-900/80'>
+    <div className='backdrop-blur-sm h-[80vh] md:h-[78vh] w-[100%] flex flex-col lg:flex-row-reverse justify-around items-center bg-gray-900/80'>
       <div>
         <h1 className='text-md md:text-xl text-center mb-2'>
           Hi! my name is{' '}
@@ -12,7 +12,7 @@ const ProfileCard = () => {
         <p className='text-md mt-6 md:text-6xl text-center flex flex-col'>
           And I'm a
           <b className='font-bold text-violet-400 text-3xl mt-4'>
-            Fullstack developer 🤠💻
+            Full Stack developer 🤠💻
           </b>
         </p>
       </div>
