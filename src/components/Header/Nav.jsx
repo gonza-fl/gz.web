2d63f611c2b8a3da7f9157af84210e41ac71e221
gonza.fl <gonzaloflamenci4@gmail.com>
2023-03-12 03:22:52 -0300

    change nav backgroud

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,9 +1,9 @@
 const Nav = ({ menuVisible }) => {
   return (
     <nav
-      className={`fixed bg-white w-[80%] md:w-[40%] xl:w-full h-full ${
+      className={`fixed backdrop-blur-md  w-[80%] md:w-[40%] xl:w-full h-full ${
         !menuVisible ? '-left-full' : 'left-0'
-      } z-40 top-0 xl:static flex-1 flex flex-col xl:flex-row justify-center xl:justify-around xl:ml-[23%] items-center transition-all duration-1000`}
+      } z-10 top-0 xl:static flex-1 flex flex-col xl:flex-row justify-center xl:justify-around xl:ml-[23%] items-center transition-all duration-1000`}
     >
       <ul className='mb-20 lg:mb-0 flex justify-center -xl:w-[80%]'>
         <li className='mr-5'>
@@ -17,7 +17,7 @@ const Nav = ({ menuVisible }) => {
           </a>
         </li>
       </ul>
-      <ul className='flex flex-col xl:flex-row gap-10 font-medium text-center w-[30%] justify-around'>
+      <ul className='flex flex-col xl:flex-row gap-10 font-medium text-white text-center w-[30%] justify-around'>
         <li>
           <a href='#'>Projects</a>
         </li>
