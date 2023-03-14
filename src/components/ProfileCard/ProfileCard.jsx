fec000201be3dbeeb07ddd5afe12aad4357dd66e
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-03-14 19:40:14 -0300

    change profile card styles

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -3,17 +3,17 @@ import PhotoProfile from './PhotoProfile';
 
 const ProfileCard = () => {
   return (
-    <div className='h-[80vh] md:h-[78vh] w-[100%] flex flex-col md:flex-row-reverse justify-around items-center'>
-      <div className='md:p-5 md:bg-gray-800/20 rounded-xl'>
+    <div className='h-[80vh] md:h-[78vh] w-[100%] flex flex-col md:flex-row-reverse justify-around items-center message-list'>
+      <div className='w-8/12 md:w-[80%] lg:w-[50%] py-5 px-3 md:p-5 nes-balloon from-left is-dark'>
         <div className='text-md md:text-xl text-center mb-2'>
           Hi! my name is{' '}
           <b className='bg-primary py-1.5 px-2 rounded-sm'>Gonzalo Flamenci</b>
         </div>
         <p className='text-xl mt-6 md:text-3xl text-center flex flex-col'>
           And I'm a
-          <h1 className='font-bold text-violet-400 text-3xl mt-4'>
+          <span className='font-bold text-violet-400 mt-4'>
             FullStack Developer 🤠💻
-          </h1>
+          </span>
         </p>
       </div>
       <PhotoProfile />
