d922abd337d41e9049aa67eda11b1f88bbe34eeb
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-03-14 19:39:22 -0300

    fix z index

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import ProfileCard from '../ProfileCard/ProfileCard';
 
 const Hero = () => {
   return (
-    <section className='h-[90vh] bg-cover bg-center bg-black/10'>
-      <div className='text-white  flex justify-center pt-10 bg-black/10'>
+    <section className='h-[80vh] absolute -z-10 lg:static'>
+      <div className='text-white  flex justify-center pt-10 bg-slate-900 rounded-bl-full rounded-tl-full md:rounded-t-none md:rounded-bl-full lg:rounded-bl-none lg:rounded-br-[100%]'>
         <ProfileCard />
       </div>
     </section>
