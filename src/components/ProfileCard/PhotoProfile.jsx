04f52909d76c5785d392d62d53b1d0b48fd150b4
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-03-16 13:50:36 -0300

    change from img element to brackground image

--- src/components/ProfileCard/PhotoProfile.jsx
+++ src/components/ProfileCard/PhotoProfile.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const PhotoProfile = () => {
   return (
-    <img
-      src='https://i.postimg.cc/NGb685dg/gdt.png'
+    <div
+      background={{ url: 'https://i.postimg.cc/NGb685dg/gdt.png' }}
       className='w-80 md:w-70 lg:w-96'
-    ></img>
+    ></div>
   );
 };
 
