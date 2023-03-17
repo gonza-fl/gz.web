08f1f28094bb8bd22ec2265d8d83e18b1d0b1a63
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-03-17 17:17:49 -0300

    fix image progile

--- src/components/ProfileCard/PhotoProfile.jsx
+++ src/components/ProfileCard/PhotoProfile.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const PhotoProfile = () => {
   return (
     <div
-      background={{ url: 'https://i.postimg.cc/NGb685dg/gdt.png' }}
-      className='w-80 md:w-70 lg:w-96'
+      style={{
+        backgroundImage: `url('https://i.postimg.cc/NGb685dg/gdt.png')`,
+      }}
+      className='w-80 h-80 lg:w-[20%] lg:h-[60%] bg-no-repeat bg-cover bg-center'
     ></div>
   );
 };
