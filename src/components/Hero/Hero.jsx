e83e86f0c0f5fca11b6848a40a73900471bd5a89
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-03-17 17:17:26 -0300

    fix hero layout

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import ProfileCard from '../ProfileCard/ProfileCard';
 
 const Hero = () => {
   return (
-    <section className='h-[80vh] absolute -z-10 lg:static'>
-      <div className='text-white  flex justify-center pt-10 bg-slate-900 rounded-bl-full rounded-tl-full md:rounded-t-none md:rounded-bl-full lg:rounded-bl-none lg:rounded-br-[100%]'>
+    <section className='relative -z-10 lg:static'>
+      <div className='h-[100vh] md:h-auto text-white md:w-[100vw] flex justify-center pt-5 bg-slate-900 rounded-bl-full rounded-tl-full md:rounded-t-none md:rounded-bl-full lg:rounded-bl-none lg:rounded-br-[100%]'>
         <ProfileCard />
       </div>
     </section>
