17dffc03cf4703a6eb55e37dd437fb6ab70eac1d
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-03-17 16:09:21 -0300

    fix z index of menu button

--- src/components/Header/MenuButton.jsx
+++ src/components/Header/MenuButton.jsx
@@ -3,7 +3,7 @@ import { RiMenu3Fill, RiCloseLine } from 'react-icons/ri';
 const MenuButton = ({ menuVisible, setMenuVisible }) => {
   return (
     <button
-      className='xl:hidden text-3xl text-white'
+      className='xl:hidden text-3xl text-white relative z-50'
       onClick={() => setMenuVisible(!menuVisible)}
     >
       {menuVisible ? <RiCloseLine /> : <RiMenu3Fill />}
