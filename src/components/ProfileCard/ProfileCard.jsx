931202a3ed46c5b2238eb937902693417195d941
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2023-03-17 17:18:08 -0300

    fix profile card layaout

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -3,11 +3,13 @@ import PhotoProfile from './PhotoProfile';
 
 const ProfileCard = () => {
   return (
-    <div className='h-[80vh] md:h-[78vh] w-[100%] flex flex-col md:flex-row-reverse justify-around items-center message-list'>
-      <div className='w-8/12 md:w-[80%] lg:w-[50%] py-5 px-3 md:p-5 nes-balloon from-left is-dark'>
+    <div className='md:h-[78vh] w-[100%] flex flex-col md:flex-row-reverse justify-around items-center'>
+      <div className='nes-balloon from-left is-dark w-8/12 md:w-[80%] lg:w-[50%] py-5 px-3 md:p-5'>
         <div className='text-md md:text-xl text-center mb-2'>
           Hi! my name is{' '}
-          <b className='bg-primary py-1.5 px-2 rounded-sm'>Gonzalo Flamenci</b>
+          <b className='bg-primary py-1.5 px-3 rounded-sm md:inline-block mt-5 lg:mt-0'>
+            Gonzalo Flamenci
+          </b>
         </div>
         <p className='text-xl mt-6 md:text-3xl text-center flex flex-col'>
           And I'm a
